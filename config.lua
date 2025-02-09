55e7cc2c26babdb3e9912a9d245134bf494c3939
WALKERIS <123303978+WALKERIS@users.noreply.github.com>
2025-02-09 02:46:02 +0200

    Add files via upload

--- config.lua
+++ config.lua
@@ -1,36 +1,35 @@
 Config = {}
 
-Config.Laikas = 40 -- sekundziu
+Config.Laikas = 40 -- seconds
 
 Config.mercedes = {
-    [1] = {pav="Akuma", spawn="akuma", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"}, -- pav privalo buti "" spawn privalo buti "" stab gali buti ir tesiog skaicius be "" bet gali buti ir su ""
-    [2] = {pav ="Fake Bugatti", spawn ="adder", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"}, -- pav privalo buti "" spawn privalo buti "" stab gali buti ir tesiog skaicius be "" bet gali buti ir su ""
-    [3] = {pav = "Amfos busiukas", spawn ="journey", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"}, -- pav privalo buti "" spawn privalo buti "" stab gali buti ir tesiog skaicius be "" bet gali buti ir su ""
-    [4] = {pav="Fake gelikas 1", spawn ="dubsta", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"}, -- pav privalo buti "" spawn privalo buti "" stab gali buti ir tesiog skaicius be "" bet gali buti ir su ""
-    [5] = {pav = "Fake gelikas 2", spawn ="dubsta2", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"} -- pav privalo buti "" spawn privalo buti "" stab gali buti ir tesiog skaicius be "" bet gali buti ir su ""
+    [1] = {pav="Brabus Rocket 900", spawn="brabusrocketsl900", stab = "naxui jo reik?", kaina = "300eur", greitis = "VISAS"},
+    [2] = {pav ="C63", spawn ="C63W205", stab = "nx jo reikia?", kaina = "500", greitis = "truputi yra"},
+    [3] = {pav = "C6320", spawn ="c6320", stab = "nx jo reikia?", kaina = "550", greitis = "bus"},
+    [4] = {pav="CLS63", spawn ="cls63", stab = "nx jo reikia?", kaina = "100", greitis = "yra yra nera bus"},
+    [5] = {pav = "E500", spawn ="e500", stab = "nx jo reikia?", kaina = "5", greitis = "zjbs ir be jo"}
 }
 
 Config.bmw = {
-    [1] = {pav="Akuma", spawn="akuma", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"}, -- pav privalo buti "" spawn privalo buti "" stab gali buti ir tesiog skaicius be "" bet gali buti ir su ""
-    [2] = {pav ="Fake Bugatti", spawn ="adder", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"}, -- pav privalo buti "" spawn privalo buti "" stab gali buti ir tesiog skaicius be "" bet gali buti ir su ""
-    [3] = {pav = "Amfos busiukas", spawn ="journey", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"}, -- pav privalo buti "" spawn privalo buti "" stab gali buti ir tesiog skaicius be "" bet gali buti ir su ""
-    [4] = {pav="Fake gelikas 1", spawn ="dubsta", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"}, -- pav privalo buti "" spawn privalo buti "" stab gali buti ir tesiog skaicius be "" bet gali buti ir su ""
-    [5] = {pav = "Fake gelikas 2", spawn ="dubsta2", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"} -- pav privalo buti "" spawn privalo buti "" stab gali buti ir tesiog skaicius be "" bet gali buti ir su ""
+    [1] = {pav="Akuma", spawn="akuma", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"},
+    [2] = {pav ="Fake Bugatti", spawn ="adder", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"},
+    [3] = {pav = "Amfos busiukas", spawn ="journey", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"},
+    [4] = {pav="Fake gelikas 1", spawn ="dubsta", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"},
+    [5] = {pav = "Fake gelikas 2", spawn ="dubsta2", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"}
 }
 
 Config.ferrari = {
-    [1] = {pav="Akuma", spawn="akuma", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"}, -- pav privalo buti "" spawn privalo buti "" stab gali buti ir tesiog skaicius be "" bet gali buti ir su ""
-    [2] = {pav ="Fake Bugatti", spawn ="adder", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"}, -- pav privalo buti "" spawn privalo buti "" stab gali buti ir tesiog skaicius be "" bet gali buti ir su ""
-    [3] = {pav = "Amfos busiukas", spawn ="journey", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"}, -- pav privalo buti "" spawn privalo buti "" stab gali buti ir tesiog skaicius be "" bet gali buti ir su ""
-    [4] = {pav="Fake gelikas 1", spawn ="dubsta", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"}, -- pav privalo buti "" spawn privalo buti "" stab gali buti ir tesiog skaicius be "" bet gali buti ir su ""
-    [5] = {pav = "Fake gelikas 2", spawn ="dubsta2", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"} -- pav privalo buti "" spawn privalo buti "" stab gali buti ir tesiog skaicius be "" bet gali buti ir su ""
+    [1] = {pav="Akuma", spawn="akuma", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"},
+    [2] = {pav ="Fake Bugatti", spawn ="adder", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"},
+    [3] = {pav = "Amfos busiukas", spawn ="journey", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"},
+    [4] = {pav="Fake gelikas 1", spawn ="dubsta", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"},
+    [5] = {pav = "Fake gelikas 2", spawn ="dubsta2", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"}
 }
 
 Config.lamborghini = {
-    [1] = {pav="Akuma", spawn="akuma", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"}, -- pav privalo buti "" spawn privalo buti "" stab gali buti ir tesiog skaicius be "" bet gali buti ir su ""
-    [2] = {pav ="Fake Bugatti", spawn ="adder", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"}, -- pav privalo buti "" spawn privalo buti "" stab gali buti ir tesiog skaicius be "" bet gali buti ir su ""
-    [3] = {pav = "Amfos busiukas", spawn ="journey", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"}, -- pav privalo buti "" spawn privalo buti "" stab gali buti ir tesiog skaicius be "" bet gali buti ir su ""
-    [4] = {pav="Fake gelikas 1", spawn ="dubsta", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"}, -- pav privalo buti "" spawn privalo buti "" stab gali buti ir tesiog skaicius be "" bet gali buti ir su ""
-    [5] = {pav = "Fake gelikas 2", spawn ="dubsta2", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"} -- pav privalo buti "" spawn privalo buti "" stab gali buti ir tesiog skaicius be "" bet gali buti ir su ""
+    [1] = {pav="Akuma", spawn="akuma", stab = "HUJOVAS", kaina = "NEIPERKAMa", greitis = "nepagaunama"},
+    [2] = {pav ="Fake Bugatti", spawn ="adder", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"},
+    [3] = {pav = "Amfos busiukas", spawn ="journey", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"},
+    [4] = {pav="Fake gelikas 1", spawn ="dubsta", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"},
+    [5] = {pav = "Fake gelikas 2", spawn ="dubsta2", stab = "HUJOVAS", kaina = "NEIPERKAMA", greitis = "nepagaunama"}
 }
-
